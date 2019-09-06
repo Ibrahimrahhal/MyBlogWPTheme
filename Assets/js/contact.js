c9b2ce6d99f504f93def0ecfd0b829303184337d
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2019-09-06 17:40:53 +0300

    done on contact page

---
+++ Assets/js/contact.js
@@ -0,0 +1,30 @@
+var lockMode = false;
+
+function myNameAnimation(){
+    
+    if(!lockMode){
+        lockMode = true;
+        $('.lineForNameAndCircleAnimations').addClass('lineForNameAndCircleAnimationsMiddle');
+        setTimeout(()=>{
+        $('.mynameAnimation').css('display','block');
+        $('.circleLogo').css('display','none');
+        // $('.lineForNameAndCircleAnimations').removeClass('lineForNameAndCircleAnimationsMiddle');
+        $('.lineForNameAndCircleAnimations').addClass('lineForNameAndCircleAnimationsRight');
+        lockMode = false;
+        },750);
+    }
+}
+
+
+$('#sideInfo').scroll((e)=>{
+    if(((e.target.scrollTop/e.target.scrollHeight) > 0.1)){
+        myNameAnimation();
+    }
+    // console.log(window.scrollY);
+});
+
+$(window).scroll((e)=>{
+    if(((window.scrollY)/($(document).height()-650)>0.2)){
+        myNameAnimation();
+    }
+});
